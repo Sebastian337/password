263025c839897c8152610d9b55f5ebaf79d1597c
Sebastian337 <aleshka_demidov@mail.ru>
2025-06-06 18:58:16 +0300

    Fix structure

--- password.py
+++ password.py
@@ -1,34 +1,34 @@
-def is_very_long(password):
-    return len(password) > 12
+MAX_LENGTH = 12
+MAX_SCORE = 10
+SCORE_CHECK = 2
 
+def is_very_long(password):
+    return len(password) > MAX_LENGTH
 
 def has_digit(password):
     return any(char.isdigit() for char in password)
 
-
 def has_lower_letters(password):
     return any(char.islower() for char in password)
-
 
 def has_upper_letters(password):
     return any(char.isupper() for char in password)
 
-
 def has_symbols(password):
     return any(not (char.isdigit() or char.isalpha()) for char in password)
 
+def main():
+    checks = [
+        is_very_long,
+        has_digit,
+        has_lower_letters,
+        has_upper_letters,
+        has_symbols
+    ]
+    
+    password = input("Введите пароль: ")
+    score = sum(SCORE_CHECK for check in checks if check(password))
+    print(f"Рейтинг пароля: {min(score, MAX_SCORE)}")
 
-checks = [
-    is_very_long,
-    has_digit,
-    has_lower_letters,
-    has_upper_letters,
-    has_symbols
-]
-
-
-password = input("Введите пароль: ")
-score = sum(2 for check in checks if check(password))
-
-
-print(f"Рейтинг пароля: {min(score, 10)}")
+if __name__ == "__main__":
+    main()
